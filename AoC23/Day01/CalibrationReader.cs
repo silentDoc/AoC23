fdb5e76e629424bcfd7b87c8f5a4875f17c266d8
silentDoc <ramon78t@yahoo.es>
2023-12-01 08:18:31 +0100

    Day 1 - Part 1 - Tests

---
+++ AoC23/Day01/CalibrationReader.cs
@@ -0,0 +1,20 @@
+﻿namespace AoC23.Day01
+{
+    public class CalibrationReader
+    {
+        List<string> calibrations = new();
+
+        public void ParseInput(List<string> input)
+            => input.ForEach(calibrations.Add);
+
+        public int ParseCalibration(string calibration)
+            => 0;
+
+        private string SolvePart1()
+            => calibrations.Select( c => ParseCalibration(c)).Sum().ToString();
+
+        public string Solve(int part)
+            => (part == 1) ? SolvePart1() : "";
+                
+    }
+}
---
+++ AoC23Tests/Day01_Tests.cs
@@ -0,0 +1,47 @@
+﻿using AoC23.Day01;
+
+namespace AoC23Tests
+{
+    [TestClass]
+    public class Day01_Tests
+    {
+        [DataTestMethod]
+        [DataRow("1abc2", 12)]
+        [DataRow("fv1abc2ffff", 12)]
+        [DataRow("3ghjg5ghjghjg", 35)]
+        [DataRow("kjhkjhkjh6mnvmnv4", 64)]
+        public void First_and_Last_2Digits(string input, int expected)
+        {
+            var reader = new CalibrationReader();
+            var result = reader.ParseCalibration(input);
+            Assert.AreEqual(expected, result);
+        }
+
+
+        [DataTestMethod]
+        [DataRow("1a3b4c2", 12)]
+        [DataRow("fv1a33b4c2ffff", 12)]
+        [DataRow("3gh6jg5ghjghjg", 35)]
+        [DataRow("kjhkjhkjh6mnv7m7nv4", 64)]
+        public void First_and_Last_MoreThan2Digits(string input, int expected)
+        {
+            var reader = new CalibrationReader();
+            var result = reader.ParseCalibration(input);
+            Assert.AreEqual(expected, result);
+        }
+
+
+        [DataTestMethod]
+        [DataRow("treb7uchet", 77)]
+        [DataRow("4hjkhkjh", 44)]
+        [DataRow("ghjgghjghjg8", 88)]
+        public void First_and_Last_Only1Digit(string input, int expected)
+        {
+            var reader = new CalibrationReader();
+            var result = reader.ParseCalibration(input);
+            Assert.AreEqual(expected, result);
+        }
+    }
+
+
+}
