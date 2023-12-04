58a64a082dcdc2b8ff30b4173ce5d4e8ef8c4d88
silentDoc <ramon78t@yahoo.es>
2023-12-04 08:23:58 +0100

    Day 4 - Part 1 Tests

---
+++ AoC23/Day04/Scratcher.cs
@@ -0,0 +1,38 @@
+﻿namespace AoC23.Day04
+{
+    public class ScratchCard
+    {
+        List<int> CardNumbers = new();
+        List<int> WinningNumbers = new();
+        int CardNum = 0;
+        public ScratchCard(string inputLine)
+        {
+            var info = inputLine.Split(":");
+            CardNum = int.Parse(info[0].Replace("Card ", ""));
+
+            var parts = info[1].Split("|");
+            CardNumbers = parts[1].Split(" ").Select(x => int.Parse(x)).ToList();
+            WinningNumbers = parts[0].Split(" ").Select(x => int.Parse(x)).ToList();
+        }
+
+        public int Points()
+            => 0;
+    }
+
+    internal class Scratcher
+    {
+        List<ScratchCard> cards = new();
+        public void ParseInput(List<string> lines)
+            => lines.ForEach(x => cards.Add(new ScratchCard(x)));
+
+        int SolvePart1()
+            => 0;
+
+        int SolvePart2()
+            => 0;
+
+
+        public int Solve(int part)
+            => part == 1 ? SolvePart1() : SolvePart2();
+    }
+}
---
+++ AoC23Tests/Day04_Tests.cs
@@ -0,0 +1,22 @@
+﻿using AoC23.Day04;
+
+namespace AoC23Tests
+{
+    [TestClass]
+    public class Day04_Tests
+    {
+        [DataTestMethod]
+        [DataRow("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53", 8)]
+        [DataRow("Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19", 2)]
+        [DataRow("Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1", 2)]
+        [DataRow("Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83", 1)]
+        [DataRow("Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36", 0)]
+        [DataRow("Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11", 0)]
+        public void Day02_Part1_Sample_Input_Test(string input, bool expected)
+        {
+            var card = new ScratchCard(input);
+
+            Assert.AreEqual(expected, card.Points());
+        }
+    }
+}
