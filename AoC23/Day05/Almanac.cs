a3bf72c14f9b899f9d664fc686399e366f482f50
silentDoc <ramon78t@yahoo.es>
2023-12-05 20:52:37 +0100

    Day 5 - Clean to reduce verbosity

--- AoC23/Day05/Almanac.cs
+++ AoC23/Day05/Almanac.cs
@@ -23,57 +23,29 @@
             => destinationStart + (queryValue - sourceStart);
     }
 
-    public class GardenValueSet
-    {
-        public long seed = -1;
-        public long soil = -1;
-        public long fert = -1;
-        public long water = -1;
-        public long light = -1;
-        public long temp = -1;
-        public long humid = -1;
-        public long location = -1;
-    }
-
     class Almanac
     {
         List<long> seeds = new();
-        List<GardenValueSet> gardenRelations = new();
-
-        List<AlmanacRule> seed2soil = new();
-        List<AlmanacRule> soil2fert = new();
-        List<AlmanacRule> fert2water = new();
-        List<AlmanacRule> water2light = new();
-        List<AlmanacRule> light2temp = new();
-        List<AlmanacRule> temp2humid = new();
-        List<AlmanacRule> humid2location = new();
+        List<AlmanacRule>[] ruleSets = new List<AlmanacRule>[7];
 
         public void ParseInput(List<string> lines)
         {
             seeds = lines[0].Replace("seeds: ", "").Split(' ')
                             .Select(x => long.Parse(x)).ToList();
 
-            var pointer = 3;
-            while (!string.IsNullOrEmpty(lines[pointer]))
-                seed2soil.Add(new AlmanacRule(lines[pointer++]));
+            var pointer = 1;
+            for (int i = 0; i < 6; i++)
+            {
+                ruleSets[i] = new();
+                pointer += 2;
+                while (!string.IsNullOrEmpty(lines[pointer]))
+                    ruleSets[i].Add(new AlmanacRule(lines[pointer++]));
+            }
+
             pointer += 2;
-            while (!string.IsNullOrEmpty(lines[pointer]))
-                soil2fert.Add(new AlmanacRule(lines[pointer++]));
-            pointer += 2;
-            while (!string.IsNullOrEmpty(lines[pointer]))
-                fert2water.Add(new AlmanacRule(lines[pointer++]));
-            pointer += 2;
-            while (!string.IsNullOrEmpty(lines[pointer]))
-                water2light.Add(new AlmanacRule(lines[pointer++]));
-            pointer += 2;
-            while (!string.IsNullOrEmpty(lines[pointer]))
-                light2temp.Add(new AlmanacRule(lines[pointer++]));
-            pointer += 2;
-            while (!string.IsNullOrEmpty(lines[pointer]))
-                temp2humid.Add(new AlmanacRule(lines[pointer++]));
-            pointer += 2;
+            ruleSets[6] = new();
             while (pointer<lines.Count)
-                humid2location.Add(new AlmanacRule(lines[pointer++]));
+                ruleSets[6].Add(new AlmanacRule(lines[pointer++]));
         }
        
         long MapValue(long queryValue, List<AlmanacRule> rules)
@@ -98,8 +70,6 @@
                 long rangeEnd = ranges[i];
                 long current = rangeStart;
 
-                // Fully process a range
-                
                 while (current <= rangeEnd)
                 {
                     result.Add(MapValue(current, rules));
@@ -157,52 +127,34 @@
 
         long SolvePart1()
         {
-            foreach (var s in seeds)
+            List<long> results = new();
+            foreach (var currentSeed in seeds)
             {
-                GardenValueSet relation = new();
-                relation.seed = s;
-                gardenRelations.Add(relation);
+                long value = currentSeed;
+                for (int r = 0; r < 7; r++)
+                    value = MapValue(value, ruleSets[r]);
+                results.Add(value);
             }
-           
-            foreach (var relation in gardenRelations)
-                relation.soil = MapValue(relation.seed, seed2soil);
-            foreach (var relation in gardenRelations)
-                relation.fert = MapValue(relation.soil, soil2fert);
-            foreach (var relation in gardenRelations)
-                relation.water = MapValue(relation.fert, fert2water);
-            foreach (var relation in gardenRelations)
-                relation.light = MapValue(relation.water, water2light);
-            foreach (var relation in gardenRelations)
-                relation.temp = MapValue(relation.light, light2temp);
-            foreach (var relation in gardenRelations)
-                relation.humid = MapValue(relation.temp, temp2humid);
-            foreach (var relation in gardenRelations)
-                relation.location = MapValue(relation.humid, humid2location);
 
-            return gardenRelations.Min(x => x.location);
+            return results.Min();
         }
         long SolvePart2()
         {
-            List<long> preppedSeeds = new();
+            List<long> ranges = new();
 
             for (int i = 0; i < seeds.Count; i++)
                 if (i % 2 == 0)
-                    preppedSeeds.Add(seeds[i]);
+                    ranges.Add(seeds[i]);
                 else
-                    preppedSeeds.Add(seeds[i-1] + seeds[i]-1);
+                    ranges.Add(seeds[i-1] + seeds[i]-1);
 
-            var soilRanges = MapRange(preppedSeeds, seed2soil);
-            var fertRanges = MapRange(soilRanges, soil2fert);
-            var waterRanges = MapRange(fertRanges, fert2water);
-            var lightRanges = MapRange(waterRanges, water2light);
-            var tempRanges = MapRange(lightRanges, light2temp);
-            var humidRanges = MapRange(tempRanges, temp2humid);
-            var locationRanges = MapRange(humidRanges, humid2location);
+            for(int i = 0;i<7; i++)
+                ranges = MapRange(ranges, ruleSets[i]);
 
-            return locationRanges.Min()-1;
-    }
+            return ranges.Min()-1;
+        }
+
         public long Solve(int part = 1)
             => part == 1 ? SolvePart1() : SolvePart2();
-
     }
 }
