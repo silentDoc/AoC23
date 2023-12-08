b743b3ff234322df7d59e5891020af1c12a4971b
silentDoc <ramon78t@yahoo.es>
2023-12-08 08:56:32 +0100

    Day 8 - Refactor to use Dictionary instead of list of linked objects

--- AoC23/Day08/DesertNavigator.cs
+++ AoC23/Day08/DesertNavigator.cs
@@ -1,76 +1,49 @@
 ﻿namespace AoC23.Day08
 {
-    class DesertNode
-    {
-        public string Name;
-        public string sLeft;
-        public string sRight;
-        public DesertNode? Left = null;
-        public DesertNode? Right = null;
-    }
-
     internal class DesertNavigator
     {
-        List<DesertNode> Nodes = new();
+        Dictionary<string, (string, string)> Map = new();
         string Instructions =  "";
 
         void ParseLine(string line)
         {
             var nodesOnly = line.Replace("= (", "").Replace(",", "").Replace(")", "");
             var nodes = nodesOnly.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-            DesertNode newNode = new();
-            newNode.Name = nodes[0];
-            newNode.sLeft = nodes[1];
-            newNode.sRight = nodes[2];
-
-            Nodes.Add(newNode);
+            Map[nodes[0]] = (nodes[1], nodes[2]);
         }
-
-        void LinkNodes()
-        {
-            foreach (var node in Nodes)
-            {
-                node.Left = Nodes.First(n => n.Name == node.sLeft);
-                node.Right = Nodes.First(n => n.Name == node.sRight);
-            }
-        }
-
         public void ParseInput(List<string> lines)
         {
             Instructions = lines[0];
             for (int i = 2; i < lines.Count; i++)
                 ParseLine(lines[i]);
-
-            LinkNodes();
         }
 
         int NavigateStorm()
         {
             var insPtr = 0;
-            DesertNode current = Nodes.First(n => n.Name == "AAA");
+            var current = "AAA";
             int steps = 0;
-            while(current.Name != "ZZZ")
+            while(current != "ZZZ")
             {
-                current = (Instructions[insPtr] == 'L') ? current.Left : current.Right;
+                current = (Instructions[insPtr] == 'L') ? Map[current].Item1 : Map[current].Item2;
                 steps++;
                 insPtr++;
                 if (insPtr == Instructions.Length)
                     insPtr = 0;
 
             }
-
             return steps;
         }
 
 
-        long GhostSteps(DesertNode ghostNode)
+        long NavigateStormGhost(string ghostNode)
         {
             var insPtr = 0;
-            DesertNode current = ghostNode;
+            var current = ghostNode;
             int steps = 0;
-            while (current.Name.Last() != 'Z')
+            while (current.Last() != 'Z')
             {
-                current = (Instructions[insPtr] == 'L') ? current.Left : current.Right;
+                current = (Instructions[insPtr] == 'L') ? Map[current].Item1 : Map[current].Item2;
                 steps++;
                 insPtr++;
                 if (insPtr == Instructions.Length)
@@ -83,12 +56,14 @@
 
         long gcd(long n1, long n2)
         {
+            // Greatest Common Divisor
             if (n2 == 0)
                 return n1;
             else
                return gcd(n2, n1 % n2);
         }
 
+        // Least Common Multiple
         long lcm(List<long> numbers)
             => numbers.Aggregate((long S, long val) => S * val / gcd(S, val));
 
@@ -96,8 +71,8 @@
         long GhostNavigator()
         {
 
-            List<DesertNode> currentNodes = Nodes.Where(x => x.Name.Last() == 'A').ToList();
-            var distances = currentNodes.Select(x => GhostSteps(x)).ToList();
+            List<string> currentNodes = Map.Keys.Where(x => x.Last() == 'A').ToList();
+            var distances = currentNodes.Select(x => NavigateStormGhost(x)).ToList();
             return lcm(distances);
         }
 
