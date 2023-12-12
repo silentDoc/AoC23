78978519dd4f4fc8c92d518958a48b77ebb73728
silentDoc <ramon78t@yahoo.es>
2023-12-12 08:17:02 +0100

    Day 12 - Readme, input and main

--- AoC23/Program.cs
+++ AoC23/Program.cs
@@ -6,9 +6,9 @@ namespace AoC23
     {
         static void Main(string[] args)
         {
-            int day = 10;
-            int part = 2;
-            bool test = false;
+            int day = 12;
+            int part = 1;
+            bool test = !false;
 
             string input = "./Input/day" + day.ToString("00");
             input += (test) ? "_test.txt" : ".txt";
@@ -29,6 +29,7 @@ namespace AoC23
                 9 => day9(input, part),
                 10 => day10(input, part),
                 11 => day11(input, part),
+                12 => day12(input, part),
                 _ => throw new ArgumentException("Wrong day number - unimplemented")
             };
             
@@ -134,6 +135,13 @@ namespace AoC23
             traveler.ParseInput(lines, part);
 
             return traveler.Solve().ToString();
+        }
+
+        static string day12(string input, int part)
+        {
+            var lines = File.ReadAllLines(input).ToList();
+
+            return "";
         }
     }
 }
