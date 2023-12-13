6664b5e2fa729deb3f23f897a882dead4a18b44c
silentDoc <ramon78t@yahoo.es>
2023-12-13 19:59:58 +0100

    Day 23 - Clean up code

--- AoC23/Day13/LavaMirrors.cs
+++ AoC23/Day13/LavaMirrors.cs
@@ -1,5 +1,4 @@
-﻿using System.Globalization;
-using System.Text;
+﻿using System.Text;
 
 namespace AoC23.Day13
 {
@@ -7,9 +6,8 @@ namespace AoC23.Day13
     {
         List<int> horizontalResults = new List<int>();
         List<int> verticalResults = new List<int>();
-
-
         List<List<string>> formations = new();
+
         public void ParseInput(List<string> lines)
         {
             List<string> activeFormation = new();
@@ -25,94 +23,71 @@ namespace AoC23.Day13
             formations.Add(activeFormation);
         }
 
-        public int CheckFormationVert(List<string> lines, int ignoreScore = -1)
-        {
-            int a = 0;
-            // Vert
-            for(int i=0; i<lines.Count-1; i++) 
-            {
-                int ini = i + 1;
-                int fin = lines.Count - ini;
-                var numElements = ini > fin ? fin : ini;
-                var iniSkip = 0;
-                if (ini > numElements)
-                    iniSkip = ini - numElements;
-
-                var subSet1 = lines.Skip(iniSkip).Take(numElements).ToList();
-                var subSet2 = lines.Skip(ini).Take(numElements).ToList();
-                subSet2.Reverse();
-
-                var peek = Enumerable.Range(0, subSet1.Count()).Select(i => subSet1[i] == subSet2[i]).ToList();
-
-                if (Enumerable.Range(0, subSet1.Count()).Select(i => subSet1[i] == subSet2[i]).Any(x => x == false))
-                    continue;
-                if((i+1)*100 != ignoreScore)
-                    return i+1;
-                
-            }
-            return -1;
-        }
-
-        public bool CheckLineHoriz(string line, int pos)
+        public bool CheckMirror<T>(List<T> elements, int pos)
         {
             int ini = pos + 1;
-            int fin = line.Length - ini;
+            int fin = elements.Count - ini;
             var numElements = ini > fin ? fin : ini;
             var iniSkip = 0;
             if (ini > numElements)
                 iniSkip = ini - numElements;
 
-            var subSet1 = line.Skip(iniSkip).Take(numElements).ToList();
-            var subSet2 = line.Skip(ini).Take(numElements).ToList();
+            var subSet1 = elements.Skip(iniSkip).Take(numElements).ToList();
+            var subSet2 = elements.Skip(ini).Take(numElements).ToList();
             subSet2.Reverse();
 
-            return !Enumerable.Range(0, subSet1.Count()).Select(i => subSet1[i] == subSet2[i]).Any(x => x == false);
+            //return subSet1.Zip(subSet2).Aggregate(true, (result, x) => result &= x.First.Equals(x.Second));
+            return !Enumerable.Range(0, subSet1.Count()).Select(i => subSet1[i].Equals(subSet2[i])).Any(x => x == false);
         }
 
-        public int CheckFormationHoriz(List<string> lines, int ignoreScore=-1)
+        public int FindMirrorRow(List<string> lines, int ignoreScore = -1)
         {
-            // Vert
-            for (int i = 0; i < lines[0].Length-1; i++)
+            for(int i=0; i<lines.Count-1; i++) 
             {
-                var check = lines.Select(x => CheckLineHoriz(x, i)).ToList();
-                if(check.Any(x => x == false)) 
+                if (!CheckMirror<string>(lines, i))
                     continue;
-                if((i+1) != ignoreScore)
-                    return i + 1;
 
+                if((i+1)*100 != ignoreScore)
+                    return i+1;
             }
             return -1;
         }
 
-        private int FormationValue(List<string> formation)
-        { 
-            int v = CheckFormationVert(formation);
-            int h = CheckFormationHoriz(formation);
-
-            h = (h>0) ? h : 0;
-            v = (v > 0) ? v*100 : 0;
-
-            horizontalResults.Add(h);
-            verticalResults.Add(v);
-
-            return h + v;
+        public int FindMirrorColumn(List<string> lines, int ignoreScore=-1)
+        {
+            // Vert
+            for (int i = 0; i < lines[0].Length-1; i++)
+            {
+                var check = lines.Select(x => CheckMirror<char>(x.ToList(), i)).ToList();
+                if (check.Any(x => x == false)) 
+                    continue;
+                if((i+1) != ignoreScore)
+                    return i + 1;
+            }
+            return -1;
         }
 
-        private int FormationValueIgnoring(List<string> formation, int pos)
+        private int GetValue(List<string> formation, int pos = -1)
         {
-            int v = CheckFormationVert(formation, verticalResults[pos]);
-            int h = CheckFormationHoriz(formation, horizontalResults[pos]);
+            int row    = pos == -1 ? FindMirrorRow(formation)    : FindMirrorRow(formation, verticalResults[pos]);
+            int column = pos == -1 ? FindMirrorColumn(formation) : FindMirrorColumn(formation, horizontalResults[pos]);
 
-            h = (h > 0) ? h : 0;
-            v = (v > 0) ? v * 100 : 0;
+            column = (column > 0) ? column : 0;
+            row = (row > 0) ? row * 100 : 0;
 
-            return h + v;
+            if (pos == -1)
+            {
+                horizontalResults.Add(column);   // We keep the original reflections in here
+                verticalResults.Add(row);     // to Ignore them for part 2
+            }
+
+            return column + row;
         }
 
         int PatchFormation(List<string> lines, int pos) 
         {
             List<StringBuilder> sbuilders = new();
-            int OriScore = FormationValue(lines);
+            GetValue(lines);  // This finds the mirror on the non patched setup, and stores the original results
             foreach (var line in lines)
                 sbuilders.Add(new StringBuilder(line));
             
@@ -129,34 +104,27 @@ namespace AoC23.Day13
                     List<string> patched = new List<string>();
                     sbuilders.ForEach(x => patched.Add(x.ToString()));
 
-                    var newScore = FormationValueIgnoring(patched, pos);
+                    var newScore = GetValue(patched, pos);
                     if (newScore > 0)
-                    {
-                        Console.WriteLine(i.ToString() + " - " + j.ToString() + " - " +newScore.ToString());
                         return newScore;
-                    }
                     else
-                        sbuilders[i][j] = oldChar;
-
+                        sbuilders[i][j] = oldChar;  // not the valid snudge, restore the position for the next iteration
                 }
             return 0;
         }
 
-        private int SolvePart1()
-            => formations.Sum(x => FormationValue(x));
-
-        private int SolvePart2()
+        private int FindMirrors(int part)
         {
+            if (part == 1)
+                return formations.Sum(x => GetValue(x));
+
             int sum = 0;
             for(int i=0; i<formations.Count; i++) 
                 sum+= PatchFormation(formations[i],i);
             return sum;
         }
-        
 
         public int Solve(int part = 1)
-            => part ==1 ? SolvePart1() : SolvePart2();
-       
-
+            => FindMirrors(part);
     }
 }
