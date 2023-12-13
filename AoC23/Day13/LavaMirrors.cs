aca3ebc5b5a7a4911f1fcc53835ab0efe18f4d1c
silentDoc <ramon78t@yahoo.es>
2023-12-13 07:39:18 +0100

    Day 12 - WIP

---
+++ AoC23/Day13/LavaMirrors.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows.Markup;
+
+namespace AoC23.Day13
+{
+
+
+    internal class LavaMirrors
+    {
+        List<List<string>> formations = new();
+        public void ParseInput(List<string> lines)
+        {
+            List<string> activeFormation = new();
+            foreach (var line in lines) 
+                if (string.IsNullOrEmpty(line))
+                {
+                    formations.Add(activeFormation);
+                    activeFormation = new();
+                }
+                else
+                    activeFormation.Add(line);
+        }
+
+
+
+        public int CheckFormation(List<string> lines)
+        { 
+            // Vert
+            for(int i=0; i<lines.Count/2; i++) 
+            {
+                var subSet1 = lines.Take(i + 1);
+                var subSet2 = lines.Skip(i + 1).Take(i + 1);
+
+            }
+        }
+            
+
+    }
+}
