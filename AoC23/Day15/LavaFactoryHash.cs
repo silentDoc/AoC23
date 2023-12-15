732400d2b4776a62f3ac7e2a1edd61a74d486443
silentDoc <ramon78t@yahoo.es>
2023-12-15 07:56:40 +0100

    Day 15 - Part 1 Completed. Reindeer Hashes

---
+++ AoC23/Day15/LavaFactoryHash.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace AoC23.Day15
+{
+    internal class LavaFactoryHash
+    {
+        List<string> sequences = new();
+
+        public void ParseInput(List<string> lines)
+            => lines[0].Split(',').ToList().ForEach(x => sequences.Add(x));
+
+        int Hash(string seq, int currentValue = 0)
+        {
+            var cv = currentValue;
+            byte[] ascii = Encoding.ASCII.GetBytes(seq);
+
+            for(int i=0;i<seq.Length;i++) 
+            {
+                cv += (int)ascii[i];
+                cv *= 17;
+                cv %= 256;
+            }
+
+            return cv;
+        }
+
+        public int Solve(int part = 1)
+            => sequences.Sum(x => Hash(x));
+
+    }
+}
--- AoC23/Program.cs
+++ AoC23/Program.cs
@@ -8,7 +8,7 @@ namespace AoC23
         {
             int day = 15;
             int part = 1;
-            bool test = !false;
+            bool test = false;
 
             string input = "./Input/day" + day.ToString("00");
             input += (test) ? "_test.txt" : ".txt";
@@ -168,7 +168,9 @@ namespace AoC23
         static string day15(string input, int part)
         {
             var lines = File.ReadAllLines(input).ToList();
-            return "";
+            Day15.LavaFactoryHash hasher = new();
+            hasher.ParseInput(lines);
+            return hasher.Solve(part).ToString();
         }
 
     }
