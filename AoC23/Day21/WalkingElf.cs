c4ce6070b2e796bf62c0afacb64b3fbbc15dc309
silentDoc <ramon78t@yahoo.es>
2023-12-21 10:04:11 +0100

    Day 21 - Part 2 WIP. I will need to accelerate p1

--- AoC23/Day21/WalkingElf.cs
+++ AoC23/Day21/WalkingElf.cs
@@ -1,10 +1,12 @@
 ﻿using AoC23.Common;
+using AoC23.Day19;
 
 namespace AoC23.Day21
 {
     internal class WalkingElf
     {
         Dictionary<Coord2D, int> Map = new();
+        int inputSize = 0;
 
         void ParseLine(string line, int row)
         {
@@ -22,9 +24,11 @@ namespace AoC23.Day21
         { 
             for(int r= 0; r < lines.Count;r++)
                 ParseLine(lines[r], r);
+
+            inputSize = lines.Count;    // According to the input, we have a 131x131 grid
         }
 
-        int TraverseMaps(int maxSteps, Coord2D start)
+        int TraverseMaps(int maxSteps, Coord2D start, int part =1)
         {
             Queue<Coord2D> active = new();
             active.Enqueue(start);
@@ -34,7 +38,18 @@ namespace AoC23.Day21
             while (active.Count > 0 && steps <=maxSteps)
             {
                 var current = active.Dequeue();
-                var neighs = current.GetNeighbors().Where(n => Map.Keys.Contains(n) && Map[n]!=-10).ToList();
+                var neighs = (part==1) ? current.GetNeighbors().Where(n => Map.Keys.Contains(n) && Map[n]!=-10).ToList() :
+                                         current.GetNeighbors().ToList();
+
+                if (part == 2)  // To support infinite grid
+                {
+                    foreach (var n in neighs)
+                    {
+                        n.x = Modulo(n.x, 131);
+                        n.y = Modulo(n.y, 131);
+                    }
+                    neighs = neighs.Where(n => Map.Keys.Contains(n) && Map[n] != -10).ToList();
+                }
 
                 foreach (var n in neighs)
                     if(!active.Contains(n))
@@ -50,13 +65,73 @@ namespace AoC23.Day21
             return active.Count();
         }
 
+        int Modulo(int x, int mod)
+            => x >0 ? x % mod : (x-x) % mod;
+
+
+        long CalcPart2()
+        {
+            // Facts and ideas to help solve this one:
+            // 
+            // We have an input that has 131x131 (inputSize) dimensions.
+            // To know the number of maximum grids we will have ==> Steps / inputSize ==> 26501365 / 131
+            // There will be some more positions to walk , we get them with remainder ==> 26501365 % 131
+            // The problem follows a chess board pattern (black/white or even/odd) ==> In one step, we are either on even/odd squares (even/odd would be sum of row+col)
+            // The nature of Manhattan distance will make that imaginary grid of grids expands like a circle (romboid):
+            //
+            //                #
+            //               ###
+            //              ##### 
+            //               ###
+            //                #
+            //
+            // So this points as sort of solution where we can find the number of positions to reach within all the original grids, multiply it by the number of grids 
+            // and then add the remainder (the extra steps we can take out of the romboid)
+            // The trick is that we can go back, for instance we can go left from grid 0 to grid -2 and then walk backwards ... so the solution has to be quadratic.
+            // If we have only 1 grid, the number of positions is finite, either even or odd
+            // If we have 5 grids (original + romboid up, down, left, right), the number of positions is 5 * 1 grid solution
+            // We can take this lineally, our total is 
+            // originalGridSteps + 2* originalGrid + 3 ...
+            // When we take remainders into account we have (remainder + num * originalGrid) => (rem), (rem + originalGridSteps), (rem + 2*originalGridSteps),
+            // The solution points to a quadratic form : a*x^2 + b*x + c = 0 ?? In order to find them, the three sequences above should be enough
+
+            // Also, bear in mind that the coords may be negative, so remainder is not helful --> we will have to use Modulo
+
+            // We look for 3 coefficients :
+            long part2Steps = 26501365;
+            long numGrids = part2Steps / ((long)inputSize);
+            long remainder = part2Steps % ((long)inputSize);
+            var start = Map.Keys.First(x => Map[x] == 0);
+            List<long> quadratic = new();
+
+            for (int coeff = 0; coeff < 3; coeff++)
+            {
+                int maxSteps = (int)  (remainder + coeff * numGrids);
+                var count = TraverseMaps(maxSteps, start, 2);
+                quadratic.Add(count);
+                Console.WriteLine(count);
+            }
+
+            // ax^2 + bx + c ==> Solving formula => x = (x = 
+
+            var c = quadratic[0];
+            var aPlusB = quadratic[1] - c;
+            var fourAPlusTwoB = quadratic[2] - c;
+            var twoA = fourAPlusTwoB - (2 * aPlusB);
+            var a = twoA / 2;
+            var b = aPlusB - a;
+
+            return a * numGrids * numGrids + b * numGrids + c;
+        }
+
+
         int FindSpots(int maxSteps)
         {
             var start = Map.Keys.First(x => Map[x] == 0);
             return TraverseMaps(maxSteps, start);
         }
 
-        public int Solve(int part = 1)
-            => FindSpots(64);
+        public long Solve(int part = 1)
+            => part == 1 ? (long)FindSpots(64) : CalcPart2();
     }
 }
--- AoC23/Day21/readme.md
+++ AoC23/Day21/readme.md
@@ -99,3 +99,65 @@ In this example, if the Elf's goal was to get exactly  `6`  more steps today, he
 However, the Elf  _actually needs to get  `64`  steps today_, and the map he's handed you is much larger than the example map.
 
 Starting from the garden plot marked  `S`  on your map,  _how many garden plots could the Elf reach in exactly  `64`  steps?_
+
+
+## --- Part Two ---
+
+The Elf seems confused by your answer until he realizes his mistake: he was reading from a  list  of his favorite numbers that are both perfect squares and perfect cubes, not his step counter.
+
+The  _actual_  number of steps he needs to get today is exactly  `_26501365_`.
+
+He also points out that the garden plots and rocks are set up so that the map  _repeats infinitely_  in every direction.
+
+So, if you were to look one additional map-width or map-height out from the edge of the example map above, you would find that it keeps repeating:
+
+```
+.................................
+.....###.#......###.#......###.#.
+.###.##..#..###.##..#..###.##..#.
+..#.#...#....#.#...#....#.#...#..
+....#.#........#.#........#.#....
+.##...####..##...####..##...####.
+.##..#...#..##..#...#..##..#...#.
+.......##.........##.........##..
+.##.#.####..##.#.####..##.#.####.
+.##..##.##..##..##.##..##..##.##.
+.................................
+.................................
+.....###.#......###.#......###.#.
+.###.##..#..###.##..#..###.##..#.
+..#.#...#....#.#...#....#.#...#..
+....#.#........#.#........#.#....
+.##...####..##..S####..##...####.
+.##..#...#..##..#...#..##..#...#.
+.......##.........##.........##..
+.##.#.####..##.#.####..##.#.####.
+.##..##.##..##..##.##..##..##.##.
+.................................
+.................................
+.....###.#......###.#......###.#.
+.###.##..#..###.##..#..###.##..#.
+..#.#...#....#.#...#....#.#...#..
+....#.#........#.#........#.#....
+.##...####..##...####..##...####.
+.##..#...#..##..#...#..##..#...#.
+.......##.........##.........##..
+.##.#.####..##.#.####..##.#.####.
+.##..##.##..##..##.##..##..##.##.
+.................................
+
+```
+
+This is just a tiny three-map-by-three-map slice of the inexplicably-infinite farm layout; garden plots and rocks repeat as far as you can see. The Elf still starts on the one middle tile marked  `S`, though - every other repeated  `S`  is replaced with a normal garden plot (`.`).
+
+Here are the number of reachable garden plots in this new infinite version of the example map for different numbers of steps:
+
+-   In exactly  `6`  steps, he can still reach  `_16_`  garden plots.
+-   In exactly  `10`  steps, he can reach any of  `_50_`  garden plots.
+-   In exactly  `50`  steps, he can reach  `_1594_`  garden plots.
+-   In exactly  `100`  steps, he can reach  `_6536_`  garden plots.
+-   In exactly  `500`  steps, he can reach  `_167004_`  garden plots.
+-   In exactly  `1000`  steps, he can reach  `_668697_`  garden plots.
+-   In exactly  `5000`  steps, he can reach  `_16733044_`  garden plots.
+
+However, the step count the Elf needs is much larger! Starting from the garden plot marked  `S`  on your infinite map,  _how many garden plots could the Elf reach in exactly  `26501365`  steps?_
